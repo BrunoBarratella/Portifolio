d1a7d2dfe985a402bc5f2382d0c90013fed53b57
Bruno Barratella <brubarratella@hotmail.com>
2023-02-04 21:50:27 -0300

    Projeto do meu Portifolio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function typeWriter (elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+}
+
+const titulo = document.querySelector('.nome');
+typeWriter(titulo);
